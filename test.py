e6d87c3f1ccaab9ee82fe71b0b0bccdfebfe8328
chengsong <990020186@qq.com>
2019-07-16 23:50:05 +0800

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        h = {}
-        for i, num in enumerate(nums):
-            if (target - num) in h:
-                return [i, h[target - num]]
-            h[num] = i
